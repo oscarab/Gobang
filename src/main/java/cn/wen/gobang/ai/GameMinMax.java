4c5704ca4e5f421a3210770db98d1a7f81a95cb4
oscarab <448575751@qq.com>
2021-04-27 19:26:42 +0800

    修复边界下棋bug

--- src/main/java/cn/wen/gobang/ai/GameMinMax.java
+++ src/main/java/cn/wen/gobang/ai/GameMinMax.java
@@ -5,6 +5,7 @@ import java.util.List;
 import cn.wen.gobang.util.Movement;
 
 public class GameMinMax extends Game{
+    private int searchNode = 0;
 
     /**
      * 极小值搜索
@@ -23,6 +24,7 @@ public class GameMinMax extends Game{
         }
         moveLists = generate();             // 生成着法
         int moveNum = moveLists.size();
+        searchNode += moveNum;
 
         for(int i = 0; i < moveNum; i++){
             move = moveLists.get(i);
@@ -60,6 +62,7 @@ public class GameMinMax extends Game{
         }
         moveLists = generate();             // 生成着法
         int moveNum = moveLists.size();
+        searchNode += moveNum;
 
         for(int i = 0; i < moveNum; i++){
             move = moveLists.get(i);
@@ -83,7 +86,13 @@ public class GameMinMax extends Game{
     }
 
     public boolean AIthink() {
+        searchNode = 0;
+        long startTime = System.currentTimeMillis();
         maxSearch(4);   // 仅搜索四层
+        outMessage("消耗时间: " + (System.currentTimeMillis() - startTime) + "ms");
+        outMessage("总搜索结点数: " + searchNode);
+        outMessage("----------结束----------");
+
         gameOut[bestMove.getX()][bestMove.getY()] = currentRole;
 
         return makeMove(bestMove.getPosition());
